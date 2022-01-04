aeb2a1eeb59b2418e55f74b3f48ddd61ef957349
Grace Yun <graceyunn@gmail.com>
2022-01-04 00:44:07 -0500

    Fixing edit entry bug

--- app/components/entry-page-three.tsx
+++ app/components/entry-page-three.tsx
@@ -56,7 +56,7 @@ function EntryPageThree({ brew_method, setBrewMethod, taste_tags, setTasteTags, 
   const listOfMethods = brewMethods.map((method: string, index: number) => {
     return (
       <button key={index} onClick={(e: MouseEvent) => handleClickMethods(e)} 
-        className={ entry && brew_method.includes(method) ? 'filter-toggled' : 'no-toggle' }>
+        className={ entry && brew_method && brew_method.includes(method) ? 'filter-toggled' : 'no-toggle' }>
         <CheckCircleOutlined style={{ display: 'none' }}/>
         {method}
       </button>
@@ -66,7 +66,7 @@ function EntryPageThree({ brew_method, setBrewMethod, taste_tags, setTasteTags, 
   const listOfTags = tasteTags.map((taste: string, index: number) => {
     return (
       <button key={index} onClick={(e: MouseEvent) => handleClickTags(e)} 
-        className={ entry && taste_tags.includes(taste) ? 'filter-toggled' : 'no-toggle' }>
+        className={ entry && taste_tags && taste_tags.includes(taste) ? 'filter-toggled' : 'no-toggle' }>
         <CheckCircleOutlined style={{ display: 'none' }}/>
         {taste}
       </button>
@@ -78,11 +78,13 @@ function EntryPageThree({ brew_method, setBrewMethod, taste_tags, setTasteTags, 
     const method = e.currentTarget.textContent
     const child = e.currentTarget.children[0] as HTMLElement
     let className = e.currentTarget.className
-    console.log(method, brew_method)
-    if (brew_method.includes(method as any)) {
+
+    if (!brew_method) setBrewMethod(new Array(1).fill(method))
+    else if (brew_method.includes(method as any)) {
       const new_methods = brew_method.filter((str: string) => str !== method)
       setBrewMethod(new_methods)
     } else setBrewMethod((prev:any) => [...prev, method])
+
     if (className === 'no-toggle') {
       e.currentTarget.className = 'filter-toggled'
       child.setAttribute('style', 'display: block; margin-right: 5px')
@@ -97,11 +99,13 @@ function EntryPageThree({ brew_method, setBrewMethod, taste_tags, setTasteTags, 
     const tag = e.currentTarget.textContent
     const child = e.currentTarget.children[0] as HTMLElement
     let className = e.currentTarget.className
-    console.log(tag, taste_tags)
-    if (taste_tags.includes(tag as any)) {
+
+    if (!taste_tags) setTasteTags(new Array(1).fill(tag))
+    else if (taste_tags.includes(tag as any)) {
       const new_taste_tags = taste_tags.filter((taste: string) => taste !== tag)
       setTasteTags(new_taste_tags)
     } else setTasteTags((prev:any) => [...prev, tag])
+    
     if (className === 'no-toggle') {
       e.currentTarget.className = 'filter-toggled'
       child.setAttribute('style', 'display: block; margin-right: 5px')
