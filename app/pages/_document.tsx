803b192d72263ca839938516d4f57f37ad993a9d
Jueun Grace Yun <graceyunn@gmail.com>
2022-07-15 13:37:52 -0400

    font

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -1,28 +1,29 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" /> 
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
-          <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&family=Volkhov:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-          <meta name="description" content="Mobile-friendly coffee tasting journal app" />
-          <meta property="og:title" content="BeanBook" />
-          <meta property="og:description" content="A minimalistic coffee tasting journal app" />
-          <meta property="og:url" content="" />
-          <meta property="og:type" content="website" />
-          <link rel="icon" href="/images/favicon.ico" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&family=Volkhov:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+						rel="stylesheet"
+					/>
+					<meta property="og:title" content="BeanBook" />
+					<meta
+						property="og:description"
+						content="A minimalistic coffee tasting journal app"
+					/>
+					<meta property="og:type" content="website" />
+					<link rel="icon" href="/images/favicon.ico" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
-export default MyDocument
+export default MyDocument;
